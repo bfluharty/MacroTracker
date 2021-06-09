39182feffc179810fe08336ac171d60dce07b532
Benjamin Fluharty <bfluharty@liberty.edu>
2021-06-09 00:03:16 -0400

    Reorg structure and adds Meal class

---
+++ Meal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MacroTracker
+{
+    public class Meal
+    {
+        public Meal()
+        {
+            Type = 'S';
+            Date = new DateTime();
+        }
+
+        public Meal(char type, DateTime date)
+        {
+            Type = type;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return "Meal [Type=" + Type +
+                ", Date=" + Date.Date + "]";
+        }
+
+        public char Type { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MealFoodMap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace MacroTracker
+{
+    public class MealFoodMap
+    {
+        public MealFoodMap()
+        {
+            Foods = new Dictionary<Food, double>();
+        }
+
+        public void AddFood(Food food, double servings)
+        {
+            Foods.Add(food, servings);
+        }
+
+        public void RemoveFood(Food food)
+        {
+            foreach (Food foodInList in Foods.Keys)
+            {
+                if (food.Name == foodInList.Name)
+                {
+                    Foods.Remove(foodInList);
+                    break;
+                }
+            }
+        }
+
+        public void EditServing(Food food, double servings)
+        {
+            RemoveFood(food);
+            AddFood(food, servings);
+        }
+
+        public override string ToString()
+        {
+            return "Meal [Foods=" + Foods.ToString() + "]";
+        }
+
+        public Dictionary<Food, double> Foods { get; }
+    }
+}
